1598d4b464ac2468d1c29d9e36af5f8f142b282e
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-01 23:12:39 +0300

    q3

--- main.js
+++ main.js
@@ -1,40 +1,5 @@
-let code = "BC";
-
-switch (code) {
-  case "BC":
-    console.log("Львівська область");
-    break;
-  case "AA":
-    console.log("Київська облість");
-    break;
-  case "AC":
-    console.log("Волинська область");
-    break;
-  case "BO":
-    console.log("Тернопільська область");
-    break;
-  case "AE":
-    console.log("Дніпропетровська область");
-    break;
-  default:
-    console.log("Такої області не існує");
-}
-
-// тест на випадок якщо такого коду не існує зі списку
-
-code = "AE";
-
-switch (code) {
-    case "BC":
-        console.log("Львівська область");
-        break;
-    case "AA":
-        console.log("Київська облість");
-        break;
-    case "AC":
-        console.log("Волинська область");
-        break;
-        default:
-        console.log("Такої області не існує");
-}
+let message = (login == 'Співробітник') ? 'Привіт':
+        (login == 'Директор') ? 'Привіт':
+        (login == '') ? 'Немає логіна':
+        '';
 
